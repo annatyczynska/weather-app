ab6dfa8173870c5a2f87d5031156ad70f20b2b0e
Anna Tyczynska <alpejka@gmail.com>
2019-10-10 22:04:50 +0200

    Updates

---
+++ precache-manifest.958d4cdc701f32f4161d50f7fce6e325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feba7e10e9b35bbbd8c2eb89b78cf5f8",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "51d980d7b20a7086d445",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1976dc7bebfb2e07e9fe",
+    "url": "/weather-app/static/css/main.98cc8126.chunk.css"
+  },
+  {
+    "revision": "51d980d7b20a7086d445",
+    "url": "/weather-app/static/js/2.b03f4ebf.chunk.js"
+  },
+  {
+    "revision": "1976dc7bebfb2e07e9fe",
+    "url": "/weather-app/static/js/main.e8d2c808.chunk.js"
+  },
+  {
+    "revision": "c2562b1d17f175861bef",
+    "url": "/weather-app/static/js/runtime-main.39aeb0d0.js"
+  }
+]);
