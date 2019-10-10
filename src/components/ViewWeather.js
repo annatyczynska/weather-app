1eb9a4ca21b75221d99c67d855afc6efc9eab1ca
Anna Tyczynska <alpejka@gmail.com>
2019-10-11 01:39:46 +0200

    fix error stats

--- src/components/ViewWeather.js
+++ src/components/ViewWeather.js
@@ -18,8 +18,8 @@ class ViewWeather extends React.Component {
         super(props);
 
         this.state = {
-            city: '',
-            country: '',
+            city: false,
+            country: false,
             latitude: '',
             longitude: '',
             temp: '',
@@ -43,7 +43,7 @@ class ViewWeather extends React.Component {
         const country = e.target.elements.country.value;
         const apiID = '05508bb378ad891b493b0c886cca7a57'
         try {
-            if (country && city)  {
+            if (country === true && city === true)  {
              const response = await
         axios.get('http://api.openweathermap.org/data/2.5/weather', {
             params: {
